f8330ccabe5dd399b24ae6bb385270ce4122556f
Florentín Pérez González <florentinpg@gmail.com>
2024-02-27 13:29:11 +0000

    fix: Context of W3CDM1

--- dist/src/common/constants/index.js
+++ dist/src/common/constants/index.js
@@ -4,5 +4,5 @@ export const ACCESS_TOKEN_EXPIRATION_TIME = 1 * 3600; // 1 hour in seconds
 export const DEFAULT_PKCE_LENGTH = 7;
 export const DEFAULT_SCOPE = "openid";
 export const OPENID_CREDENTIAL_AUTHZ_DETAILS_TYPE = "openid_credential";
-export const CONTEXT_VC_DATA_MODEL_1 = ["https://www.w3.org/ns/credentials/v1"];
+export const CONTEXT_VC_DATA_MODEL_1 = ["https://www.w3.org/2018/credentials/v1"];
 export const CONTEXT_VC_DATA_MODEL_2 = ["https://www.w3.org/ns/credentials/v2"];
