b353b88d7011b8d92f885756f397c9b17a450ced
Florentín Pérez González <florentinpg@gmail.com>
2024-02-27 13:27:02 +0000

    fix: Context of W3CDM1

--- src/common/constants/index.ts
+++ src/common/constants/index.ts
@@ -4,7 +4,7 @@ export const ACCESS_TOKEN_EXPIRATION_TIME = 1 * 3600 // 1 hour in seconds
 export const DEFAULT_PKCE_LENGTH = 7;
 export const DEFAULT_SCOPE = "openid";
 export const OPENID_CREDENTIAL_AUTHZ_DETAILS_TYPE = "openid_credential";
-export const CONTEXT_VC_DATA_MODEL_1 = ["https://www.w3.org/ns/credentials/v1"];
+export const CONTEXT_VC_DATA_MODEL_1 = ["https://www.w3.org/2018/credentials/v1"];
 export const CONTEXT_VC_DATA_MODEL_2 = ["https://www.w3.org/ns/credentials/v2"];
 export type JWA_ALGS =
   "HS256" | "HS384" | "HS512" | "RS256" | "RS384" | "RS512" | "ES256" |
